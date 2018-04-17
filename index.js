8d4a45257d73cd22dff34276f56046c57b285362
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-04-17 17:13:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var bot = new Discord.Client();
 
 bot.on("ready", () => {
     bot.user.setStatus('online')
-    bot.user.setActivity('Система обновлена успешно', { type: 'PLAYING' });
+    bot.user.setActivity('Обновление командных модулей', { type: 'PLAYING' });
     var servers = bot.guilds.array().map(g => g.name).join('.');
     console.log('Бот готов');
 });
