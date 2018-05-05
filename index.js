f6365f33096814129fcb8a412ba8dcd3721081cd
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-05-05 21:41:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -210,23 +210,6 @@ bot.on("message", async message => {
     if(message.author.bot) return;
     if(message.channel.type === "dm") return;
 
-    if (message.content == ".+gamemath") {
-        var embed = new Discord.RichEmbed()
-        .setTitle("Игра в Вычисление ➕ ➖")
-        .setThumbnail("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYLN1gSWD0BSCBRGWXBsNy4tl-Pe4sGl0JcoQdCCc4ELv8UTo3") 
-        .addField("Для того чтобы начать игру существует следующая команда", "Вычисление старт")
-        .setDescription("У этой игры существуют следующие уровни сложности:                            1.Легкий                       2.Средний                            3.Сложный                                 4.Максимальный           5.Экстерн                                                                                                                                             Для того чтобы получить новый пример существует команда Пример=(ваш уровень)                                                                                     Бот даст пример с той сложностью,которую вы указали")
-        .setFooter("Раздел:Игры Helper bot 2018 ©")
-        .addField("Для того чтобы указать уровень сложности существует следующая команда", "Вычисление=(ваш уровень)")     
-        .setColor(embedColor.color)
-        message.channel.sendEmbed(embed);
-    }
-});
-
-bot.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
-
     if (message.content == "Вычисление старт") {
         var embed = new Discord.RichEmbed()
         .setTitle("Укажите уровень сложности")
