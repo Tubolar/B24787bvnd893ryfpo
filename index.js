5a76320d09ebbde091e709cb830821eadc614932
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-05-05 21:06:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
 const Discord = require ("discord.js");
-const embedColor = require("./embedcolor.json")
+const embedColor = require("./embedcolor.json");
+const fs = require("fs");
+const botconfig = require("./botconfig.json");
+bot.commands = new Discord.Collection();
 
 var bot = new Discord.Client();
+
+fs.readdir("./commands/", (err,files) =>{
+
+
+    if(err) console.log(err);
+
+
+
+let jsfile = files.filter(f => f.split(".").pop() === "js")
+if(jsfile.length <= 0) {
+    console.log("Couldn't find commands");
+    return;
+}
+
+jsfile.forEach((f, i) =>{
+    let props = require (`./commands/${f}`);
+    console.log(`${f} loaded!`);
+    bot.commands.set(props.help.name, props);
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(bot, message, args);
+      
+});
+
+});
 
 bot.on("ready", () => {
     bot.user.setStatus('online')
