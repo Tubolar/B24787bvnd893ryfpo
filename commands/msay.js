11ac83dd3cbeee773cf366e15375f31a98a00137
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-05-05 21:55:20 +0300

    Create msay.js

---
+++ commands/msay.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (bot, message, args) => {
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Недостаточно прав.")
+
+    const sayEmbed = new Discord.RichEmbed()
+    .setTitle(message.author.username + " сказал: ")
+    .setDescription(args.join(" "))
+    .setColor("#15f153")
+    message.delete().catch();
+
+    message.channel.send(sayEmbed)
+
+
+
+};
+
+module.exports.help = {
+    name: "msay"
+}   
