0ddbd9ca9445d3ae7b294bbd59b2d9a70b719e43
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-05-08 19:40:28 +0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,13 +1,17 @@
 const Discord = require("discord.js");
- 
+
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Недостаточно прав.")
     let botmessage = args.join(" ");
     message.delete().catch();
-    message.channel.send(botmessage);
 
-};
-
+    let modRole = message.guild.roles.find("name", "ZONTIk");
+        if(message.member.roles.has(modRole.id)) {
+        message.channel.send(botmessage)
+        } else {
+         message.reply(",у Вас недостаточно прав.")
+        }
+    };
 module.exports.help = {
     name: "say"
 }
