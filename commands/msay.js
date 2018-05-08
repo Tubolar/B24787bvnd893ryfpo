0aa038217b700843f1a9426caa7eb68f177d590a
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-05-08 19:41:20 +0300

    Update msay.js

--- commands/msay.js
+++ commands/msay.js
@@ -1,19 +1,28 @@
-const Discord = require("discord.js")
-
+const Discord = require("discord.js");
+const embedColor = require("../embedColor.json");
+const ercolor = require("../embedColor.json")
+ 
 module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Недостаточно прав.")
 
     const sayEmbed = new Discord.RichEmbed()
     .setDescription(args.join(" "))
-    .setColor("#15f153")
+    .setColor(embedColor.color)
     message.delete().catch();
 
-    message.channel.send(sayEmbed)
 
-
-
-};
+    let modRole = message.guild.roles.find("name", "ZONTIk");
+        if(message.member.roles.has(modRole.id)) {
+        message.channel.send(sayEmbed)
+        } else {
+            message.reply({embed:{
+                "description": `**${message.author}**,произошла ошибка\nПричина:**нет достаточных прав**`,
+                "color": embedColor.errorcolor
+              }
+            })
+        }
+}
 
 module.exports.help = {
     name: "msay"
-}   
+}
