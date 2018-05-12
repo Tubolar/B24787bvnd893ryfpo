97484fcf2d6bcbb0b7095d4d7ce33755a19b9e2f
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-05-12 19:48:04 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,9 +1,14 @@
 const Discord = require("discord.js");
+const embedColor = require("../embedColor.json");
  
 module.exports.run = async (bot, message, args) => {
+
+    let user = message.mentions.users.first() || message.author;
+    if(!user) return message.reply(",не могу найти участника");
+
     var embed = new Discord.RichEmbed()
-    .setImage(message.author.avatarURL)
-    .setColor("0x00cc99")
+    .setImage(user.displayAvatarURL)
+    .setColor(embedColor.color)
     message.channel.sendEmbed(embed);
 }
 
