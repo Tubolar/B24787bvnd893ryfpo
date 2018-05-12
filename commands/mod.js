5133ae8c5fe8dc61f215428f0b6314d7f4f4146f
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-05-12 16:36:43 +0300

    Create mod.js

---
+++ commands/mod.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+ 
+module.exports.run = async (bot, message, args) => {
+    var modEmb = new Discord.RichEmbed()
+    .setTitle("Команды для Модераторов сервера")
+    .setDescription("```css\n,mute\n,unmute\,tempmute\n,warn\n,clear")
+    .setFooter("Модуль:Модерация.")
+    .setTimestamp(new Date(Date))
+
+    let modRole = message.guild.roles.find("name", "Moderator");
+    if(message.member.roles.has(modRole.id)) {
+        message.channel.send(modEmb);
+    } else {
+        message.reply("у вас недостаточно прав")
+    }
+}
+
+module.exports.help = {
+    name: "report"
+}
