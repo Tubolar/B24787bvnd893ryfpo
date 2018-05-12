fbe39801fa947724523a59a692a41fc016b111a5
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-05-12 18:38:10 +0300

    Update mod.js

--- commands/mod.js
+++ commands/mod.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const errorcolor = require("../embedcolor.json");
  
 module.exports.run = async (bot, message, args) => {
     var modEmb = new Discord.RichEmbed()
@@ -6,12 +7,16 @@ module.exports.run = async (bot, message, args) => {
     .setDescription("```css\n,mute\n,unmute\n,tempmute\n,warn\n,clear```")
     .setFooter("Модуль:Модерация.")
     .setTimestamp()
+    
+    var erEmbed = new Discord.RichEmbed()
+    .setDescription(`${message.author},у вас недостаточно прав.`)
+    .setColor(errorcolor.error);
 
     let modRole = message.guild.roles.find("name", "Moderator");
     if(message.member.roles.has(modRole.id)) {
         message.channel.send(modEmb);
     } else {
-        message.reply("у вас недостаточно прав")
+        message.channel.send(erEmbed)
     }
 }
 
