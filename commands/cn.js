8c9e5292867c5b6e15dbf42496424edca9e90781
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-05-22 23:55:37 +0300

    Create cn.js

---
+++ commands/cn.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    let modRole = message.guild.roles.find("name", "Moderator");
+    if(message.member.roles.has(modRole.id)) {
+    let menUser = message.mentions.members.first() || message.guild.members.get(args[0]);
+    menUser.setNickname(args[2])
+    let reason = args.join(" ").slice(1);
+    message.channel.send("Ник изменён.")
+    } else {
+        message.reply("у Вас недостаточно прав.")
+    }  
+} 
+
+module.exports.help = {
+    name: "поменяй,ник"
+}
