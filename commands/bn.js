2fc383f062b9f7f151eae1acedc5e01f1d2d1479
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-06-10 17:18:39 +0300

    Create bn.js

---
+++ commands/bn.js
@@ -0,0 +1,14 @@
+
+const Discord = require("discord.js");
+ 
+module.exports.run = async (bot, message, args) => {
+    let newn = args[2];
+    let reason = args.join(" ").slice(1);
+    if(!newn) return message.reply("укажите Ваш новый ник.")
+    message.member.setNickname(newn)
+    message.channel.send(message.author + ",Ваш ник успешно изменён")
+}
+
+module.exports.help = {
+    name: "т,можно"
+}
