18c03cd04f4ac47a81e29f19d7072f1b43f5cef4
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-06-10 22:57:27 +0300

    Update sec.js

--- commands/sec.js
+++ commands/sec.js
@@ -1,20 +1,22 @@
 const Discord = require("discord.js");
  
 module.exports.run = async (bot, message, args) => {
+    let TOPRole = message.guild.roles.find(`name`, "TOP")
     let embed = new Discord.RichEmbed()
-    embed.setAuthor(message.author, message.author.displayAvatarURL)
+    embed.setAuthor(message.author.username, message.author.displayAvatarURL)
     embed.setTitle("Цвет роли TOP успешно изменён")
     embed.setFooter("Group TOP-GAMERS")
     embed.setTimestamp(new Date())
     embed.addField("Новый цвет", TOPRole.color)
     embed.setColor("#862d86");
     let builder = new Discord.RichEmbed()
-    .setAuthor(message.author, message.author.displayAvatarURL)
+    .setAuthor(message.author.username, message.author.displayAvatarURL)
     .setDescription(`${message.author},у вас недостаточно прав.`)
     .setFooter(`Group TOP-GAMERS`)
-    .setTimestamp(new Date());
+    .setTimestamp(new Date())
+    .setColor("#8d4646")
     let color = args[3]
-    let TOPRole = message.guild.roles.find('name', "TOP")
+    if(!color) return message.channel.send(`${message.author},Вы не указали цвет роли.`)
     if(message.member.roles.has(TOPRole.id)) {
         TOPRole.setColor(color).then(message.channel.send(embed))
 } else {
@@ -23,6 +25,5 @@ module.exports.run = async (bot, message, args) => {
 }
 
 module.exports.help = {
-    name: "т,поменяй"
+    name: "т,смени"
 }
-   
