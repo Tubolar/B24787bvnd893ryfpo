5d53ad9d9cbce6dd972072db454ebb41b5790047
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-06-10 21:55:40 +0300

    Update dcndx.js

--- commands/dcndx.js
+++ commands/dcndx.js
@@ -5,6 +5,8 @@ module.exports.run = async (bot, message, args) => {
     let newn = args[2];
     let reason = args.join(" ").slice(1);
     if(!newn) return message.reply("укажите Ваш новый ник.")
+    let modRole = message.guild.roles.find('name', "TOP")
+    if(message.member.roles.has(modRole.id)) return message.reply("у меня недостаточно прав,так как Ваша роль находится выше моей")
     message.member.setNickname(newn)
     message.channel.send(message.author + ",Ваш ник успешно изменён")
 }
