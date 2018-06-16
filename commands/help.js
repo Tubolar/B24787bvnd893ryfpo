128d680b02d4b316ef1c13e09e0549c6d6653ba1
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-06-16 21:05:48 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
- 
+
 module.exports.run = async (bot, message, args) => {
+   
     var embed = new Discord.RichEmbed()
-    .setDescription("```yaml\n•.+help\n•.+link\n•.+avatar\n•.+aboutme\n•.+serverinfo\n•.+game\n•.+roles\n•.+rules```")
-    .setColor("0x00cc99")
+    .setDescription("```ini\n++cmd [имя_команды]\n++mcmds [имя_модуля]\n++mdls [список_модулей]```")
+    .setColor(0x0ffff)
     .setFooter("Helper bot ©2018")
     message.channel.sendEmbed(embed);
 };
