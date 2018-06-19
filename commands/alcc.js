0b9cd641c1e1b54f168cc3baa3156510e085cec9
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-06-19 23:42:52 +0300

    Update alcc.js

--- commands/alcc.js
+++ commands/alcc.js
@@ -6,22 +6,27 @@ module.exports.run = async (bot, message, args) => {
     const modRole = message.guild.roles.find('name', "ZONTIk") 
     function cRole() {
         roles.forEach((role) => {
+            let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
             let aRole = message.guild.roles.find('name', "Admin")
             let tRole = message.guild.roles.find("name", "Group");
             let dRole = message.guild.roles.find("name", "Посетитель");
             let rdRole = message.guild.roles.find("name", "Гость");
-            let cArole = ["#080707","#1f1f2e", "#310125", "#191f20"];
-            let ctRole = ["#000066","#0d0d0d","#333399","#4040bf"];
-            let cdRole = ["#95291d","#d53b2a","#d94f3f","#aa2f22"];
-            let crdRole = ["#217961","#1b6451","#268c71","#31b491"];
-            let cctRoler = Math.floor((Math.random() * ctRole.length)); 
-            let ccdRoler = Math.floor((Math.random() * cdRole.length)); 
-            let ccrdRoler = Math.floor((Math.random() * crdRole.length)); 
-            let cAroler = Math.floor((Math.random() * cArole.length))
-            aRole.edit({color: cArole[cAroler]})
-          .then(tRole.edit({color: ctRole[cctRoler]}))
-          .then(dRole.edit({color: cdRole[ccdRoler]}))
-          .then(rdRole.edit({color: crdRole[ccrdRoler]}))
+            //let cArole = ["#080707","#1f1f2e", "#310125", "#191f20"];
+            //let ctRole = ["#000066","#0d0d0d","#333399","#4040bf"];
+            //let cdRole = ["#95291d","#d53b2a","#d94f3f","#aa2f22"];
+            //let crdRole = ["#217961","#1b6451","#268c71","#31b491"];
+            //let cctRoler = Math.floor((Math.random() * ctRole.length)); 
+            //let ccdRoler = Math.floor((Math.random() * cdRole.length)); 
+            //let ccrdRoler = Math.floor((Math.random() * crdRole.length)); 
+            //let cAroler = Math.floor((Math.random() * cArole.length))
+            //aRole.edit({color: cArole[cAroler]})
+          //.then(tRole.edit({color: ctRole[cctRoler]}))
+          //.then(dRole.edit({color: cdRole[ccdRoler]}))
+          //.then(rdRole.edit({color: crdRole[ccrdRoler]}))
+            aRole.edit({color: random})
+            .then(tRole.edit({color: random}))
+            .then(dRole.edit({color: random}))
+            .then(rdRole.edit({color: random}))
           .catch(e => {
             return message.channel.sendMessage("Произошла ошибка,зайдите в консоль.");
           });
