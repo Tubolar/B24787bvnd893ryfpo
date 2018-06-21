80f96a8bd44eda91f62233e54b2e06c47285611e
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-06-21 08:59:03 +0300

    Update efind.js

--- commands/efind.js
+++ commands/efind.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     var target = args.join(" ")
     var emoji = message.guild.emojis.find('name', target)
-    message.channel.send(emoji)
+    message.channel.send(`**Резултат**:${emoji}`)
 }
 
 module.exports.help = {
