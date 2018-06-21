defbd2822d1c51a231eba099c3f939bf2ff3ce49
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-06-21 11:10:15 +0300

    Update alcc.js

--- commands/alcc.js
+++ commands/alcc.js
@@ -11,18 +11,18 @@ module.exports.run = async (bot, message, args) => {
             let tRole = message.guild.roles.find("name", "Group");
             let dRole = message.guild.roles.find("name", "Посетитель");
             let rdRole = message.guild.roles.find("name", "Гость");
-            //let cArole = ["#080707","#1f1f2e", "#310125", "#191f20"];
-            //let ctRole = ["#000066","#0d0d0d","#333399","#4040bf"];
-            //let cdRole = ["#95291d","#d53b2a","#d94f3f","#aa2f22"];
-            //let crdRole = ["#217961","#1b6451","#268c71","#31b491"];
-            //let cctRoler = Math.floor((Math.random() * ctRole.length)); 
-            //let ccdRoler = Math.floor((Math.random() * cdRole.length)); 
-            //let ccrdRoler = Math.floor((Math.random() * crdRole.length)); 
-            //let cAroler = Math.floor((Math.random() * cArole.length))
-            //aRole.edit({color: cArole[cAroler]})
-          //.then(tRole.edit({color: ctRole[cctRoler]}))
-          //.then(dRole.edit({color: cdRole[ccdRoler]}))
-          //.then(rdRole.edit({color: crdRole[ccrdRoler]}))
+            let cArole = ["#080707","#1f1f2e", "#310125", "#191f20"];
+            let ctRole = ["#000066","#0d0d0d","#333399","#4040bf"];
+            let cdRole = ["#95291d","#d53b2a","#d94f3f","#aa2f22"];
+            let crdRole = ["#217961","#1b6451","#268c71","#31b491"];
+            let cctRoler = Math.floor((Math.random() * ctRole.length)); 
+            let ccdRoler = Math.floor((Math.random() * cdRole.length)); 
+            let ccrdRoler = Math.floor((Math.random() * crdRole.length)); 
+            let cAroler = Math.floor((Math.random() * cArole.length))
+            aRole.edit({color: cArole[cAroler]})
+            .then(tRole.edit({color: ctRole[cctRoler]}))
+            .then(dRole.edit({color: cdRole[ccdRoler]}))
+            .then(rdRole.edit({color: crdRole[ccrdRoler]}))
             aRole.edit({color: random})
             tRole.edit({color: random})
             dRole.edit({color: random})
