a7392c2ec2b946e4079b520cd9518924abfc402a
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-07-04 02:24:00 +0300

    Update dcndx.js

--- commands/dcndx.js
+++ commands/dcndx.js
@@ -2,8 +2,12 @@
 const Discord = require("discord.js");
  
 module.exports.run = async (bot, message, args) => {
+ 
+    let modRole = message.guild.roles.find('name', "TOP")
+    if(message.member.roles.has(modRole.id)) return message.reply("у меня недостаточно прав,так как Ваша роль находится выше моей.")
+    let mR = message.guild.roles.find('name', "ZONTIk")
+    if(message.member.roles.has(mR.id)) return message.reply("у меня недостаточно прав,так как Вы являетесь владельцем этого сервера.")
     let newn = args[2];
-    let reason = args.join(" ").slice(1);
        if(!newn) { return message.channel.send('Укажите Ваш новый никнейм.')
     .then(() => {
       message.channel.awaitMessages(nickname => nickname = message.content, {
@@ -22,10 +26,7 @@ module.exports.run = async (bot, message, args) => {
     message.member.setNickname(newn) 
     message.channel.send(message.author + ",Ваш ник успешно изменён")
 }
-    let modRole = message.guild.roles.find('name', "TOP")
-    if(message.member.roles.has(modRole.id)) return message.reply("у меня недостаточно прав,так как Ваша роль находится выше моей.")
-    let mR = message.guild.roles.find('name', "ZONTIk")
-    if(message.member.roles.has(mR.id)) return message.reply("у меня недостаточно прав,так как Вы являетесь владельцем этого сервера.")
+ 
     message.member.setNickname(newn)
     message.channel.send(message.author + ",Ваш ник успешно изменён")
 }
