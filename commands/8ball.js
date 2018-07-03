580dcdd254174f2413a0a79ce4f77acdab699ae2
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-07-03 14:45:26 +0300

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,12 +3,11 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
 
 
-    if(!args[2]) return message.reply("пожалуйста,задайте полный вопрос.");
     let replies = ["Да.", "Нет.", "Я не знаю ツ", "Возможно", "Скорее всего", "Это факт", "Спросите меня позже..."];
 
     let result = Math.floor((Math.random() * replies.length));
-    let question = args.slice(1).join(" ");
-
+    let question = args.join(" ")
+    if(!question) return message.channel.send("Пожалуйста,задайте вопрос.")
     let ballEmbed = new Discord.RichEmbed()
     .setAuthor(message.author.tag)
     .setColor("0x00cc99")
