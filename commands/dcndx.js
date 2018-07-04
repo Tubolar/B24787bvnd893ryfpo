80d4350790bad0a5b639af6d265ad16150337610
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-07-04 12:27:59 +0300

    Update dcndx.js

--- commands/dcndx.js
+++ commands/dcndx.js
@@ -7,21 +7,7 @@ module.exports.run = async (bot, message, args) => {
     let mR = message.guild.roles.find('name', "ZONTIk")
     if(message.member.roles.has(mR.id)) return message.reply("у меня недостаточно прав,так как Вы являетесь владельцем этого сервера.")
     let newn = args[2];
-       if(!newn) { return message.channel.send('Укажите Ваш новый никнейм.')
-    .then(() => {
-      message.channel.awaitMessages(nickname => nickname = message.content, {
-        max: 1,
-        time: 30000,
-        errors: ['time'],
-      })
-      .then((collected) => {
-          message.member.setNickname(message.content)
-          message.channel.send(`Ваш никнейм успешно изменён`);
-        })
-    }).catch(() => {
-        message.channel.send("Никнейм не был установлен,так как я не получил его течении 30 секунд");
-      });
-    }
+       if(!newn) return message.channel.send('Укажите Ваш новый никнейм.')
     message.member.setNickname(newn) 
     message.channel.send(message.author + ",Ваш ник успешно изменён.")
 }
