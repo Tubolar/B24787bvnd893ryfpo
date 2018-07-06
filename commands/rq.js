3da7fbe90d9436adbd441fe5ce41cd74f4e94660
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-07-06 15:35:40 +0300

    Update rq.js

--- commands/rq.js
+++ commands/rq.js
@@ -1,11 +1,11 @@
 const Discord = module.require("discord.js");
  
 module.exports.run = async (bot, message, args) => {
-   let nRole = message.guild.roles.find('name', "Новый участник/New member")
    let gRole = message.guild.roles.find('name', "Гость")
-   if(message.member.roles.has(nRole.id)) {
+   if(message.channel.id = "462942047813500928") {
+        if(message.member.roles.has(gRole.id)) return;
        let schannel = bot.channels.find('name', "spam")
-       message.member.addRole(gRole.id).then(message.member.removeRole(nRole.id))
+       message.member.addRole(gRole.id)
        .then(message.channel.send(`Пользователь ${message.author} получил доступ к некоторым текстовым и голосовым каналам.`))
        .then(schannel.send(`${message.author},Вы получили доступ к некоторым текстовым и голосовым каналам.`))
    } 
