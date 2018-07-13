34f4daeba6f50a36be8a0796ad120e6e474fcdfc
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-07-13 21:45:17 +0300

    Update rq.js

--- commands/rq.js
+++ commands/rq.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
    let gRole = message.guild.roles.find('name', "Гость")
    if(message.channel.id = "462942047813500928") {
         if(message.member.roles.has(gRole.id)) return;
-       let schannel = bot.channels.find('name', "spam")
+       let schannel = bot.channels.find('name', "хлам")
        message.member.addRole(gRole.id)
        .then(message.channel.send(`Пользователь ${message.author} получил доступ к некоторым текстовым и голосовым каналам.`))
        .then(schannel.send(`${message.author},Вы получили доступ к некоторым текстовым и голосовым каналам.`))
