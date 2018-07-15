21d393c87cbea927b9dcc6d9cd799622295e1afe
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-07-15 14:38:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const botconfig = require("./botconfig.json");
 var bot = new Discord.Client();
 const cmds = require('./cmds_strings.json');
 const cmd = require('./cmd_strings.json');
+const ENVIRONMENT = new Discord.WebhookClient(process.env.ENVIRONMENT_ID, process.env.ENVIRONMENT_POTION)
 bot.commands = new Discord.Collection();
 
 
@@ -51,6 +52,7 @@ bot.on("ready", () => {
     var channel = bot.channels.get('448546642375868436');
     channel.sendMessage("Бот `Helper` снова в сети.");
     console.log('Бот готов');
+	ENVIRONMENT.send("Working!")
 });
 
 bot.on("message", async message => {
