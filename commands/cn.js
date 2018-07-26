25c855473c2ff27ad2a1e350b951069fe34a87b5
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-07-26 03:27:33 +0300

    Update cn.js

--- commands/cn.js
+++ commands/cn.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
     let modRole = message.guild.roles.find("name", "Moderator");
     if(message.member.roles.has(modRole.id)) {
     let menUser = message.mentions.members.first() || message.guild.members.get(args[0]);
-    menUser.setNickname(args[3])
+    menUser.setNickname(args.join(" ").slice(25))
     let reason = args.join(" ").slice(1);
     message.channel.send("Ник изменён.")
     } else {
@@ -13,5 +13,5 @@ module.exports.run = async (bot, message, args) => {
 } 
 
 module.exports.help = {
-    name: "т,поменяй"
+    name: "т,теперь"
 }
