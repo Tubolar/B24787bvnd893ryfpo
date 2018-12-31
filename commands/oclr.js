8e64576f43df6c84ea63ca5e56dde532c25de348
Tubolar <37476425+Tubolar@users.noreply.github.com>
2018-12-31 14:11:41 +0300

    Update oclr.js

--- commands/oclr.js
+++ commands/oclr.js
@@ -2,15 +2,17 @@ const Discord = require("discord.js");
  
 module.exports.run = async (bot, message, args) => {
     let aRole = message.guild.roles.find('name', "Admin")
-            let tRole = message.guild.roles.find("name", "Elite");
-            let dRole = message.guild.roles.find("name", "Посетитель");
-            let rdRole = message.guild.roles.find("name", "Гость");
+            let tRole = message.guild.roles.find("name", "High");
+            let dRole = message.guild.roles.find("name", "Medium");
+            let rdRole = message.guild.roles.find("name", "Low");
+            let btRole = message.guild.roles.find("name", "Бот");
             let mRole = message.guild.roles.find('name', "ZONTIk")
             if(message.member.roles.has(mRole.id)) {
                 aRole.setColor("#030303")
-                tRole.setColor("#030303")
-                dRole.setColor("#95291D")
-                rdRole.setColor("#217961")
+                tRole.setColor("#180303")
+                dRole.setColor("#4BA5D4")
+                rdRole.setColor("#927954")
+                btRole.setColor("#206694")
             } else {
                 message.reply("у Вас недостаточно прав.")
             }
