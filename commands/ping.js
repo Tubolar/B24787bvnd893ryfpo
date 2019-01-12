72d0233097c572ed2398ab74956d2a017c3cae19
Tubolar <37476425+Tubolar@users.noreply.github.com>
2019-01-12 23:33:34 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
  
 module.exports.run = async (bot, message, args) => {
     var pingl = new Discord.RichEmbed()
-    .setDescription(`**${message.author}**,мой отклик составляет ${Math.round(bot.ping)} мc`)
+    .setDescription(`**${message.author}**,получаю информацию...`)
     .setColor(0x00e6e6)
     message.channel.send(pingl).then(sent => sent.edit(new Disoord.RichEmbed().setColor(0x00e6e6).addField("Хостинг", Math.round(bot.ping), true).addField("API", sent.createdTimestamp - message.createdTimestamp))))
 }
